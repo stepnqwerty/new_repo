f398d6aaa1e6aba466bec984744fe6acbdcb51f5
MetaMask1 <163715010+stepnqwerty@users.noreply.github.com>
2025-09-29 13:52:15 +0300

    Create 72_branch.py

---
+++ 72_branch.py
@@ -0,0 +1,31 @@
+import turtle
+
+def draw_branch(branch_length, t):
+    if branch_length > 5:
+        t.forward(branch_length)
+        t.right(20)
+        draw_branch(branch_length - 15, t)
+        t.left(40)
+        draw_branch(branch_length - 15, t)
+        t.right(20)
+        t.backward(branch_length)
+
+def main():
+    screen = turtle.Screen()
+    screen.bgcolor("white")
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.color("brown")
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.width(3)
+
+    draw_branch(75, t)
+
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    main()
